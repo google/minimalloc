0d172f013cece9f98652ed008251f1b22450a82f
Michael D. Moffitt <moffitt@google.com>
2023-09-21 15:36:11 +0000

    Updates the script to use the proper benchmark directory.

--- scripts/run-challenging.sh
+++ scripts/run-challenging.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for input in benchmarks/asplos2024/*; do
+for input in benchmarks/challenging/*; do
   cmd="./minimalloc --capacity=1048576 --input=$input --output=out.txt"
   runtime=$( (eval "$cmd") 2>&1 )
   printf "%s %8s sec.\n" "$input" "$runtime"
